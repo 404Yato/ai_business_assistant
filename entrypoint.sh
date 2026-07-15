5b45ee197973dfa886be227fb5d089ec7ea8659a
Cristian Tapia <cristianxd2022@gmail.com>
2026-07-15 18:06:40 -0400

    fix: modified environment variables references

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,35 +6,41 @@ cat >/etc/odoo/odoo.conf <<EOF
 addons_path = /usr/lib/python3/dist-packages/odoo/addons,/mnt/extra-addons
 data_dir = /var/lib/odoo
 
-db_host = ${HOST}
-db_port = ${PORT}
-db_user = ${USER}
-db_password = ${PASSWORD}
-db_name = ${DATABASE}
+db_host = ${PGHOST}
+db_port = ${PGPORT}
+db_user = ${PGUSER}
+db_password = ${PGPASSWORD}
+db_name = ${PGDATABASE}
+
+http_port = ${PORT}
 EOF
 
+
 echo "Esperando PostgreSQL..."
-until PGPASSWORD="$PASSWORD" psql \
-    -h "$HOST" \
-    -p "$PORT" \
-    -U "$USER" \
-    -d "$DATABASE" \
+
+until PGPASSWORD="$PGPASSWORD" psql \
+    -h "$PGHOST" \
+    -p "$PGPORT" \
+    -U "$PGUSER" \
+    -d "$PGDATABASE" \
     -c "SELECT 1;" >/dev/null 2>&1
 do
     sleep 2
 done
 
-INITIALIZED=$(PGPASSWORD="$PASSWORD" psql \
-    -h "$HOST" \
-    -p "$PORT" \
-    -U "$USER" \
-    -d "$DATABASE" \
+
+INITIALIZED=$(PGPASSWORD="$PGPASSWORD" psql \
+    -h "$PGHOST" \
+    -p "$PGPORT" \
+    -U "$PGUSER" \
+    -d "$PGDATABASE" \
     -tAc "SELECT COUNT(*) FROM pg_tables WHERE schemaname='public' AND tablename='ir_module_module';")
+
 
 if [ "$INITIALIZED" = "1" ]; then
     echo "Base de datos ya inicializada."
     exec odoo
 else
     echo "Inicializando Odoo..."
-    exec odoo -d "$DATABASE" -i base --without-demo=all
+    exec odoo -d "$PGDATABASE" -i base --without-demo=all
 fi
